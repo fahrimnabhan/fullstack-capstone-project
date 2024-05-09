dea473ca8d73a39ab608f8ee591febc15606cb90
fahrimnabhan <fm.nabhan@rocketmail.com>
2024-05-09 02:49:42 -0400

    Implement back-end in login component

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,15 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
+import { post } from '../../../../giftlink-backend/routes/authRoutes';
 
 function LoginPage() {
 
     //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContect();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app')
+        }
+    }, [navigate])
 
     // insert code here to create handleLogin function and include console.log
     const handleLogin = async () => {
-        console.log("Inside handleLogin");
+        
+        try{
+            e.preventDefault();
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+                method: 'POST',
+
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': bearerToken ? `Bearer ${bearerToken}` : '',
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                })
+            });
+
+            const json = await response.json();
+            console.log('Json', json);
+            if (json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', json.userName);
+                sessionStorage.setItem('email', json.userEmail);
+
+                setIsLoggedIn(true);
+
+                navigate('/app');
+            } else {
+                document.getElementById("email").value="";
+                document.getElementById("password").value="";
+                setIncorrect("Wrong password. Try again.")
+                setTimeout(() => {
+                    setIncorrect("");
+                }, 2000);
+            }
+        } catch (e) {
+            console.log("Error fetching details: " + e.message);
+        }
     }
 
         return (
@@ -17,26 +68,23 @@ function LoginPage() {
         <div className="row justify-content-center">
           <div className="col-md-6 col-lg-4">
             <div className="login-card p-4 border rounded">
-              <h2 className="text-center mb-4 font-weight-bold">Login</h2>
-
-          {/* insert code here to create input elements for the variables email and  password */}
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email</label>
-            <input
-                id="email"
-                type="text"
-                className="form-control"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            </div>
-
-            <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
+                <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+                {/* insert code here to create input elements for the variables email and  password */}
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input
+                        id="email"
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
                 <p className="mt-4 text-center">
                     New here? <a href="/app/register" className="text-primary">Register Here</a>
                 </p>
-
             </div>
           </div>
         </div>
